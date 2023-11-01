ae1f0eca2f5ab112b316a8edcd4b9ac07e6f5ef3
Abdallh Elhosary <abdallhelhosary@gmail.com>
2023-11-01 02:24:55 +0200

    Products Crud

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -3,12 +3,10 @@ import categoryReducer from './categoryReducer'
 import brandReducer from './brandReducer'
 import subCategoryReducer from './subCategoryReducer'
 import productReducer from './productReducer'
-import allTypesReducer from "./allTypesReducer"
 
 export default combineReducers ({
     allCategory: categoryReducer,
     allBrand: brandReducer,
     subCategory: subCategoryReducer,
     allProduct: productReducer,
-    allTypes:allTypesReducer,
 })
